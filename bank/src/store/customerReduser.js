d3ea921975f9f6cc8960f6a7754de6500e9db6ea
Marat Gumerov <gumerovm74@gmail.com>
2021-10-23 23:15:22 +0300

    add : thunk

--- bank/src/store/customerReduser.js
+++ bank/src/store/customerReduser.js
@@ -4,11 +4,16 @@ const defaultState = {
 
 const ADD_CUSTOMER = 'ADD_CUSTOMER';
 const REMOVE_CUSTOMER = 'REMOVE_CUSTOMER';
+const ADD_MANY_CUSTOMERS = 'ADD_MANY_CUSTOMERS';
+
 
 export const customerReduser = (state = defaultState, action) => {
   switch (action.type) {
     case ADD_CUSTOMER:
       return {...state, customers: [...state.customers, action.payLoad]}
+    case ADD_MANY_CUSTOMERS:
+      return {...state, customers: [...state.customers, ...action.payLoad]}
+
     case REMOVE_CUSTOMER:
       return {...state, customers: state.customers.filter(customer => customer.id !== action.payLoad)}
 
@@ -18,4 +23,5 @@ export const customerReduser = (state = defaultState, action) => {
 }
 
 export const addCustomerAction = (payLoad) => ({type: ADD_CUSTOMER, payLoad});
+export const addManyCustomersAction = (payLoad) => ({type: ADD_MANY_CUSTOMERS, payLoad});
 export const removeCustomerAction = (payLoad) => ({type: REMOVE_CUSTOMER, payLoad});
--- bank/src/store/index.js
+++ bank/src/store/index.js
@@ -1,7 +1,8 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {cashReduser} from "./cashReduser";
 import {customerReduser} from "./customerReduser";
 import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 
 const rootReducer = combineReducers({
@@ -9,4 +10,4 @@ const rootReducer = combineReducers({
   customers: customerReduser
 })
 
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
